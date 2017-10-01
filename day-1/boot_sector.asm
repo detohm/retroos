f663c48343f36b69b50ba6f65331dde4ae1d3391
Ohm Attaphong <ohm.attaphong@gmail.com>
2017-10-01 22:19:09 +0700

    day 1 | Create boot sector for day-1 experiment

---
+++ day-1/boot_sector.asm
@@ -0,0 +1,11 @@
+; Infinite loop to prevent CPU from executing other routines
+loop $
+
+; Fill in with zero
+times 510-($-$$) db 0
+
+; Magic bit sequences for identifying boot sector
+db 0x55
+db 0xAA
+
+
