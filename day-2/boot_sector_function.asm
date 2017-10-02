3f03c57c52f3891756299c79ed7431eddf597fd4
Ohm Attaphong <ohm.attaphong@gmail.com>
2017-10-02 23:18:47 +0700

    day 2 | Try function call with global offset

---
+++ day-2/boot_sector_function.asm
@@ -0,0 +1,15 @@
+[org 0x7C00]
+
+; Invoke BIOS interupt call
+mov ah, 0x0E
+mov al, [test_func]
+int 0x10
+
+test_func:
+    db "O"
+
+; zero padding
+times 510-($-$$) db 0
+; Magic bit sequences for identifying boot sector
+db 0x55
+db 0xAA
