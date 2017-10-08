89ffa788ffac0710e3ba3b41d2ed95ca244ed96c
Ohm Attaphong <ohm.attaphong@gmail.com>
2017-10-08 23:43:22 +0700

    day 4 | Finish the print string function with push/pop stack

--- day-4/boot_sector_print_string.asm
+++ day-4/boot_sector_print_string.asm
@@ -1,9 +1,45 @@
-[org 0x7C00]
+[org 0x7c00]
+
+; Try store 'C' in register
+mov al, 'C'
 
 mov bx, HELLO_MSG
 call FN_PRINT_STRING
 
+; Try to print 'C' which declared before calling the function,
+; It will not print put the 'C' if it's manipulated in the function.
+; But we use pusha & popa to save current state of all register and restore back when return of function call
+; Therefore, the system print 'C' correctly.
+mov ah, 0x0e
+int 0x10
+
+jmp $
+
+
+; Function
+FN_PRINT_STRING:
+  pusha
+
+FN_PRINT_STRING_START:
+
+  mov al, [bx]
+  cmp al, 0
+  je FN_PRINT_STRING_DONE
+
+  mov ah, 0x0e
+  int 0x10
+
+  add bx, 1
+  jmp FN_PRINT_STRING_START
+
+FN_PRINT_STRING_DONE:
+  popa
+  ret
+
+
+; Data
 HELLO_MSG:
   db 'hello world',0
 
-
+times 510 - ($ - $$) db 0
+dw 0xaa55
