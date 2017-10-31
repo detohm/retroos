97dc0ebdd8635ef03bf2b2c9358894574c9525eb
Ohm Attaphong <ohm.attaphong@gmail.com>
2017-11-01 00:43:53 +0700

    day-10 | Update code for screen scrolling

--- day-10/drivers/screen.c
+++ day-10/drivers/screen.c
@@ -61,7 +61,7 @@ int print_char (char character, int col, int row, char attribute_byte) {
   }
 
 
-  //offset = handle_scrolling(offset);
+  offset = handle_scrolling(offset);
   set_cursor_offset(offset);
   return offset;
 }
@@ -92,6 +92,33 @@ void print_at (char* message, int col, int row) {
 void print(char* message){
   print_at(message, -1, -1);
 }
+
+int handle_scrolling(int cursor_offset) {
+  if (cursor_offset < MAX_ROWS*MAX_COLS*2){
+    return cursor_offset;
+  }
+
+  int i;
+  for(i=1; i<MAX_ROWS; i++){
+    memory_copy(
+      get_screen_offset(0,i) + VIDEO_ADDRESS,
+      get_screen_offset(0,i-1) + VIDEO_ADDRESS,
+      MAX_COLS*2);
+
+  }
+
+
+  char* last_line = get_screen_offset(0,MAX_ROWS - 1) + VIDEO_ADDRESS;
+  for(i=0; i < MAX_COLS*2; i++){
+    last_line[i] = 0;
+  }
+
+  cursor_offset -= 2*MAX_COLS;
+
+  return cursor_offset;
+
+}
+
 
 void clear_screen(){
   int row = 0;
--- day-10/kernel/kernel.c
+++ day-10/kernel/kernel.c
@@ -1,5 +1,5 @@
 #include "../drivers/screen.h"
-
+#include "./util.h"
 void some_useless_function() {
 
 }
@@ -9,5 +9,15 @@ void main() {
   // char* video_memory = (char*) 0xb8000;
   // *video_memory = 'X';
   clear_screen();
-  print("Hello");
+
+
+  int i = 0;
+  for (i=0; i<24; i++){
+    char str[255];
+    int_to_ascii(i, str);
+    print_at(str,0,i);
+  }
+
+  print_at("This text forces the kernel to scroll. Row 0 will disappear. ", 60, 24);
+  print("And with this text, the kernel will scroll again, and row 1 will disappear too!");
 }
